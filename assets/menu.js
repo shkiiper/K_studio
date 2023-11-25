832c0e6c8929f979d8cfdf20a3fd64a6b3e5298e
User <user@MacBook-Pro.local>
2023-11-25 12:36:31 +0600

    second

--- assets/menu.js
+++ assets/menu.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelector('.formClose').addEventListener('click', ()=>{
-    document.querySelector('.mainFormPopup').classList.add('d-none')
-    document.querySelector('body').style.overflow = 'auto'
-  })
-  document.querySelector('.formOpen').addEventListener('click', ()=>{
-    document.querySelector('.mainFormPopup').classList.remove('d-none')
-    document.querySelector('body').style.overflow = 'hidden'
-  })
-})
+// document.addEventListener('DOMContentLoaded', ()=>{
+//   document.querySelector('.formClose').addEventListener('click', ()=>{
+//     document.querySelector('.mainFormPopup').classList.add('d-none')
+//     document.querySelector('body').style.overflow = 'auto'
+//   })
+//   document.querySelector('.formOpen').addEventListener('click', ()=>{
+//     document.querySelector('.mainFormPopup').classList.remove('d-none')
+//     document.querySelector('body').style.overflow = 'hidden'
+//   })
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded сработал');
+  
+  const formClose = document.querySelector('.formClose');
+  const formOpen = document.querySelector('.formOpen');
+  const mainFormPopup = document.querySelector('.mainFormPopup');
+
+  console.log(formClose, formOpen, mainFormPopup);
+
+  formClose.addEventListener('click', () => {
+    console.log('Кнопка закрытия нажата');
+    mainFormPopup.classList.add('d-none');
+    document.querySelector('body').style.overflow = 'auto';
+  });
+
+  formOpen.addEventListener('click', () => {
+    console.log('Кнопка открытия нажата');
+    mainFormPopup.classList.remove('d-none');
+    document.querySelector('body').style.overflow = 'hidden';
+  });
+});
--- assets/telegram.js
+++ assets/telegram.js
@@ -1,13 +1,47 @@
+// const TOKEN = '6506225535:AAE0_AQf7ljB2l9Vo3LINrrUm4Y4jy8hjbg'
+// const CHAT_ID = '-1002040526890'
+// const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   let formCall = document.querySelector('.mainForm').addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     let name = document.querySelector('#name-form2-8').value
+//     let phone = document.querySelector('#number-form2-8').value
+//     if(!!phone){
+//       axios.post(URI_API, {
+//         chat_id: CHAT_ID,
+//         parse_mode: 'HTML',
+//         text: `Новая заявка с сайта
+// Имя: ${name || 'Не указано'}
+// Телефон: ${phone}
+// `
+//       }).then((res)=>{
+//         if(res.status === 200){
+//           document.querySelector('#number-form2-8').value = 'Вы успешно оставили заявку' 
+//           setTimeout(()=>{
+//             document.querySelector('#number-form2-8').value = ''
+//             document.querySelector('.mainFormPopup').classList.add('d-none')
+//             document.querySelector('body').style.overflow = 'auto'
+//           }, 100)
+//         }
+//       })
+//     } else {
+//       console.log('заполните поля')
+//     }
+//   })
+// })
+
 const TOKEN = '6506225535:AAE0_AQf7ljB2l9Vo3LINrrUm4Y4jy8hjbg'
 const CHAT_ID = '-1002040526890'
 const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
 
 document.addEventListener('DOMContentLoaded', () => {
-  let formCall = document.querySelector('.mainForm').addEventListener('submit', (e) => {
+  let formCall = document.querySelector('.pop-up-form-netpeak-site').addEventListener('submit', (e) => {
     e.preventDefault()
 
-    let name = document.querySelector('#name-form2-8').value
-    let phone = document.querySelector('#number-form2-8').value
+    let name = document.querySelector('#name-6').value
+    let phone = document.querySelector('#phone-6').value
     if(!!phone){
       axios.post(URI_API, {
         chat_id: CHAT_ID,
@@ -18,10 +52,10 @@ document.addEventListener('DOMContentLoaded', () => {
 `
       }).then((res)=>{
         if(res.status === 200){
-          document.querySelector('#number-form2-8').value = 'Вы успешно оставили заявку' 
+          document.querySelector('#phone-6').value = 'Вы успешно оставили заявку' 
           setTimeout(()=>{
-            document.querySelector('#number-form2-8').value = ''
-            document.querySelector('.mainFormPopup').classList.add('d-none')
+            document.querySelector('#phone-6').value = ''
+            document.querySelector('.pop-up-form-netpeak-site').classList.add('d-none')
             document.querySelector('body').style.overflow = 'auto'
           }, 100)
         }
